3ae2d8bc0b7405a61677188eaf46614a5b7174b2
Malkiel Asher <masher@syr.edu>
2020-07-17 13:14:02 -0400

    Updated Personal Activity Profile Pic

--- app/src/main/java/com/example/groceryshare/PersonalActivity.java
+++ app/src/main/java/com/example/groceryshare/PersonalActivity.java
@@ -2,32 +2,58 @@ package com.example.groceryshare;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class PersonalActivity extends AppCompatActivity {
-    String profilePic, name, address, store, payment, rewards, others, userId;
+    String profilePic;
+    String name;
+    String address;
+    String store;
+    String payment;
+    String rewards;
+    String others;
+    String userID;
+
+    private static final String TAG = "PersonalActivity";
+    int TAKE_IMAGE_CODE = 10001;
+    String DISPLAY_NAME = null;
+    String PROFILE_IMAGE_URL = null;
 
     EditText storeInput;
     EditText paymentInput;
     EditText rewardsInput;
     EditText othersInput;
-    EditText name;
-    ImageView image;
+    EditText nameInput;
+    ImageView ProfileImage;
     EditText addressInput;
     Button submitButton;
 
@@ -47,61 +73,39 @@ public class PersonalActivity extends AppCompatActivity {
         //databaseBuyers = FirebaseDatabase.getInstance().getReference("buyers");
 
         img = findViewById(R.id.GoBackIcon);//defines the back button image
-        address = (TextView) findViewById(R.id.addressid);
-        address.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                address.setCursorVisible(true);
-                address.setFocusableInTouchMode(true);
-                address.setInputType(InputType.TYPE_CLASS_TEXT);
-                address.requestFocus();
-            }
-        });
-        name = (TextView) findViewById(R.id.nameid);
-        image = (ImageView) findViewById(R.id.imageid);
+        addressInput = (EditText) findViewById(R.id.addressid);
+        nameInput = (EditText) findViewById(R.id.nameid);
+        ProfileImage = (ImageView) findViewById(R.id.imageid);
 
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            Log.d(TAG, "onCreate: " + user.getDisplayName());
+//            if (user.getDisplayName() != null) {
+//                addressInput.setText(user.getUid());
+//                displayNameEditText.setText(user.getDisplayName());
+//                displayNameEditText.setSelection(user.getDisplayName().length());
+//            }
+            if (user.getPhotoUrl() != null) {
+                Glide.with(this)
+                        .load(user.getPhotoUrl())
+                        .into(ProfileImage);
+            }
+        }
 
         FirebaseDatabase.getInstance().getReference().child("Buyers").child(userID)
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         newBuyerCreds buyer = dataSnapshot.getValue(newBuyerCreds.class);
-                        address.setText(buyer.getAddress());
-                        name.setText(buyer.getFirstName() + " " + buyer.getLastName());
-                        //Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), buyer.getProfilePhoto());
-                        //image.setImageBitmap(bitmap);
+                        addressInput.setText(buyer.getAddress());
+                        nameInput.setText(buyer.getFirstName() + " " + buyer.getLastName());
                     }
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
-
                     }
                 });
-        databaseBuyers = FirebaseDatabase.getInstance().getReference("Buyers");
-        DatabaseReference buyerAddress = databaseBuyers.child("-MCDy7cXDPYgWvLh7SVV").child("address");
-
-        buyerAddress.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                address = dataSnapshot.getValue().toString();
-                addressInput.setText(address);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
-
-        buyerAddress.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                address = dataSnapshot.getValue().toString();
-                addressInput.setText(address);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
 
         img = findViewById(R.id.GoBackIcon);//defines the back button image
 
@@ -135,11 +139,111 @@ public class PersonalActivity extends AppCompatActivity {
     }
 
 
-
     //used to navigate back to the previous screen
     public void goBack(View v) {
         Intent intent = new Intent(this, OrderActivity.class);
         startActivity(intent);
     }
 
+    public void updateProfile(final View view) {
+        view.setEnabled(false);
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+                .setDisplayName(DISPLAY_NAME)
+                .build();
+
+        firebaseUser.updateProfile(request)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        view.setEnabled(true);
+                        Toast.makeText(PersonalActivity.this, "Successfully updated profile", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        view.setEnabled(true);
+                        Log.e(TAG, "onFailure: ", e.getCause());
+                    }
+                });
+    }
+
+    public void handleImageClick(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, TAKE_IMAGE_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TAKE_IMAGE_CODE) {
+            switch (resultCode) {
+                case RESULT_OK:
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    ProfileImage.setImageBitmap(bitmap);
+                    handleUpload(bitmap);
+            }
+        }
+    }
+
+    private void handleUpload(Bitmap bitmap) {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final StorageReference reference = FirebaseStorage.getInstance().getReference()
+                .child("profileImages")
+                .child(uid + ".jpeg");
+
+        reference.putBytes(baos.toByteArray())
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        getDownloadUrl(reference);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "onFailure: ",e.getCause() );
+                    }
+                });
+    }
+
+    private void getDownloadUrl(StorageReference reference) {
+        reference.getDownloadUrl()
+                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Log.d(TAG, "onSuccess: " + uri);
+                        setUserProfileUrl(uri);
+                    }
+                });
+    }
+
+    private void setUserProfileUrl(Uri uri) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+                .setPhotoUri(uri)
+                .build();
+
+        user.updateProfile(request)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(PersonalActivity.this, "Updated successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(PersonalActivity.this, "Profile image failed...", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
