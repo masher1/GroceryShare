4c77bf6c68e21636166ad4940a2360df68b93d3d
Malkiel Asher <masher@syr.edu>
2020-07-29 20:13:45 -0400

    Fixed Email verification for password

--- app/src/main/java/com/example/groceryshare/ui/login/ResetPassword.java
+++ app/src/main/java/com/example/groceryshare/ui/login/ResetPassword.java
@@ -1,8 +1,5 @@
 package com.example.groceryshare.ui.login;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,17 +7,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.groceryshare.BuyerHomeScreen;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.groceryshare.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ResetPassword extends AppCompatActivity {
-Button submit;
-String emailAddress;
-EditText emailET;
-Boolean moveForward;
+    Button submit;
+    String emailAddress;
+    EditText emailET;
+    Boolean moveForward;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,33 +36,37 @@ Boolean moveForward;
                 Intent intent = new Intent(ResetPassword.this, LoginActivity.class);
                 emailAddress = emailET.getText().toString();
                 moveForward = checkEmail(emailAddress);
-                if (moveForward==true) {
+                if (moveForward == true) {
                     startActivity(intent);
                 }
             }
         });
     }
+
     //used to navigate back to the previous screen
     public void goBack(View v) {
-        Intent intent = new Intent(this, BuyerHomeScreen.class);
-        startActivity(intent);
+        finish();
     }
-    public Boolean checkEmail(String email){
-        Boolean advance;
-        if (email!=""){
-            advance=true;
+
+    public Boolean checkEmail(String email) {
+        Boolean advance = false;
+        Pattern pattern = Pattern.compile("[a-z0-9]+([-+._][a-z0-9]+){0,2}@.*?(\\.(a(?:[cdefgilmnoqrstuwxz]|ero|(?:rp|si)a)|b(?:[abdefghijmnorstvwyz]iz)|c(?:[acdfghiklmnoruvxyz]|at|o(?:m|op))|d[ejkmoz]|e(?:[ceghrstu]|du)|f[ijkmor]|g(?:[abdefghilmnpqrstuwy]|ov)|h[kmnrtu]|i(?:[delmnoqrst]|n(?:fo|t))|j(?:[emop]|obs)|k[eghimnprwyz]|l[abcikrstuvy]|m(?:[acdeghklmnopqrstuvwxyz]|il|obi|useum)|n(?:[acefgilopruz]|ame|et)|o(?:m|rg)|p(?:[aefghklmnrstwy]|ro)|qa|r[eosuw]|s[abcdeghijklmnortuvyz]|t(?:[cdfghjklmnoprtvwz]|(?:rav)?el)|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw])\\b){1,2}");
+        Matcher emailVerified = pattern.matcher(email);
+        if (!emailVerified.find()) {
+            emailET.setText("");
+            advance = false;
+            Toast.makeText(this, "Please enter a valid email address", Toast.LENGTH_SHORT).show();
+        } else {
             FirebaseAuth.getInstance().sendPasswordResetEmail(emailAddress)
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()) {
+                                Toast.makeText(ResetPassword.this, "You will get an email shorty if this account exists!", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
-        }
-        else{
-            advance=false;
-            Toast.makeText(this, "Please enter an email address", Toast.LENGTH_SHORT).show();
+            advance = true;
         }
         return advance;
     }
